9a6ac21bd734223e93027629a074df4c579b3503
amiynarh <aminalawal186@yahoo.com>
2025-04-19 20:43:04 +0100

    chore: replace favicon ICO with new SVG file and update response headers

--- src/pages/favicon.ico.ts
+++ src/pages/favicon.ico.ts
@@ -2,11 +2,11 @@ import { readFile } from 'node:fs/promises'
 import path from 'node:path'
 import type { APIRoute } from 'astro'
 
-const faviconPath = path.resolve('src/images/favicon.ico')
+const faviconPath = path.resolve('src/images/favicon_icon2.svg')
 
 export const GET: APIRoute = async () => {
     const ico = await readFile(faviconPath)
     return new Response(ico, {
-        headers: { 'Content-Type': 'image/x-icon' },
+        headers: { 'Content-Type': 'image/svg+xml' },
     })
 }
