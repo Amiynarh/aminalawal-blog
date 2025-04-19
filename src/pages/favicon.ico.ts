258e9e4ec95e321baed67915f092a6d48fd46090
amiynarh <aminalawal186@yahoo.com>
2025-04-19 18:53:53 +0100

    chore: update favicon handling to serve ICO format instead of SVG

--- src/pages/favicon.ico.ts
+++ src/pages/favicon.ico.ts
@@ -2,11 +2,11 @@ import { readFile } from 'node:fs/promises'
 import path from 'node:path'
 import type { APIRoute } from 'astro'
 
-const faviconPath = path.resolve('src/images/favicon_icon.svg')
+const faviconPath = path.resolve('src/images/favicon.ico')
 
 export const GET: APIRoute = async () => {
-    const svg = await readFile(faviconPath)
-    return new Response(svg, {
-        headers: { 'Content-Type': 'image/svg+xml' },
+    const ico = await readFile(faviconPath)
+    return new Response(ico, {
+        headers: { 'Content-Type': 'image/x-icon' },
     })
 }
